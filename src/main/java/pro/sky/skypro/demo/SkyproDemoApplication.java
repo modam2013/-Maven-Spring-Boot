aedcaf59531a851f9357b26714a0f1bf02ef1ab5
Аня <grigorevna.antoninka@mail.ru>
2023-03-14 11:36:02 +0700

    началтный коммит

---
+++ src/main/java/pro/sky/skypro/demo/SkyproDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skypro/demo/SkyproDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
